e8db3513fe417fc4f6a9a68706b890e926030f21
Yyl <yuanlinplays@gmail.com>
2026-01-12 10:23:21 +0800

    Android xml idk

--- test/app/src/main/java/com/example/test/MainActivity.java
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnSOS = findViewById(R.id.btnSOS);
+        btnSOS.bringToFront();
+        FrameLayout container = findViewById(R.id.fragmentContainer);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnSOS.setOnClickListener(v -> {
+            container.setVisibility(View.VISIBLE);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainer, new SOSFragment())
+                    .addToBackStack(null)
+                    .commit();
         });
     }
 }
---
+++ test/app/src/main/java/com/example/test/SOSFragment.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SOSFragment extends Fragment {
+    public SOSFragment(){}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_sos, container, false);
+    }
+}
